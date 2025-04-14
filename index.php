cca73f15793848268069613978c25c57cff0f62e
Beatriz <1416962@senati.pe>
2025-04-14 10:38:48 -0500

    cambios

--- index.php
+++ index.php
@@ -1,35 +1,30 @@
 <?php include 'config.php'; ?>
-<style>
-    table{
-        ba
-    }
- </style>   
- <h1>Lista de Usuarios</h1>
- 
- <a href="create.php">Agregar Nuevo</a>
 
- <table border="i">
-  <tr>
-  <td> id</td>
-  <td> name </td>
-  <td> lastname </td>
-  <td> Actions </td>
+<h1>Lista de usuarios</h1>
 
-</tr>
+<a href="create.php">Agregar Nuevo</a>
 
-<?php
-$result = $conexion->query("SELECT * FROM usuario");
-
-while($row = $result->fetch_assoc()){
- echo "
+<table border="1">
     <tr>
-  <td> $row[id]</td>
-  <td> $row[name] </td>
-  <td> $row[lastname] </td>
-  <td><a href= 'a' </td>
+        <td>id</td>
+        <td>name</td>
+        <td>lastName</td>
+        <td>Acciones</td>
+    </tr>
+    
+    <?php
+    
+    $result = $conexion->query("SELECT * FROM usuario");
 
-</tr> ";
-}
+    while($row = $result->fetch_assoc()){
+        echo "
+            <tr>
+                <td>$row[id]</td>
+                <td>$row[name]</td>
+                <td>$row[lastname]</td>
+                <td><a href='#'>Editar</a> | <a href='#'>Eliminar</a></td>
+            </tr> ";
+    }
 
-?>
+    ?>
 </table>
